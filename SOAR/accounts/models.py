b1970da5ddf1f418247506b99d5182d0fd3acb2d
Princess Althea <andicolminas1@gmail.com>
2025-12-05 22:06:54 +0800

    Fix course field schema mismatch - change from CharField to ForeignKey

--- SOAR/accounts/models.py
+++ SOAR/accounts/models.py
@@ -146,7 +146,13 @@ class User(AbstractUser):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     student_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
-    course = models.CharField(max_length=100, blank=True)
+    course = models.ForeignKey(
+        'organization.Program',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='users'
+    )
     year_level = models.PositiveSmallIntegerField(null=True, blank=True)
     profile_picture = models.ImageField(
         upload_to=user_profile_upload_path,
